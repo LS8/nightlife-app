f8d21ea237a00a635fa34645bebc1eb49bbad338
LS8 <lennartschoettker@hotmail.com>
2017-07-16 19:43:13 +0200

    use registerUser to add the user from the request to the db

---
+++ controllers/user/register.js
@@ -0,0 +1,20 @@
+const User = require('../../models/User');
+
+module.exports = (req, res) => {
+  const userToRegister = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  User.registerUser(userToRegister, (err, alreadyRegistered, success) => {
+    if (err) {
+      res.json({ success: false, msg: 'Failed to register', status: 1 });
+    } else if (alreadyRegistered) {
+      res.json({ success: false, msg: 'Username already exists', status: 2 });
+    } else if (success) {
+      res.json({ success: true, msg: 'Account registered', status: 0 });
+    } else {
+      res.json({ success: false, msg: 'Oops, something went wrong..', status: 3 });
+    }
+  });
+}
