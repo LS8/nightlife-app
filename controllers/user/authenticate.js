43d90504505c3d5fcab8a12296bbe0e31a033808
LS8 <lennartschoettker@hotmail.com>
2017-07-25 15:27:23 +0200

    use real secret

--- controllers/user/authenticate.js
+++ controllers/user/authenticate.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 const User = require('../../models/User');
+const secret = require('../../secrets').authSecret;
 
 module.exports = (req, res) => {
   const username = req.body.username;
@@ -16,7 +17,7 @@ module.exports = (req, res) => {
           res.json({ success: false, msg: 'Error' });
         } else if (isMatch) {
           // create token
-          const token = jwt.sign(user, 'secret', { expiresIn: 604800 });
+          const token = jwt.sign(user, secret, { expiresIn: 604800 });
           res.json({
             success: true, msg: 'Correct password', token: `JWT ${token}`, user: {
               id: user._id,
