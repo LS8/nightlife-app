68b484da69c309eff2115d2afec3962e5e55f676
LS8 <lennartschoettker@hotmail.com>
2017-07-25 15:26:59 +0200

    require authentication for index route

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,14 @@
+const passport = require('passport');
 const express = require('express');
 const Router = express.Router();
 
 // Controllers
 const register = require('../controllers/user/register');
 const authenticate = require('../controllers/user/authenticate');
+const index = require('../controllers/user/index');
 
 Router.post('/register', register);
 Router.post('/authenticate', authenticate);
+Router.get('/', passport.authenticate('jwt', { session: false }), index);
 
 module.exports = Router;
