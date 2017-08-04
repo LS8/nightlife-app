d9bb47fa2902b58e7b2828c0b9c8bd76796012ad
LS8 <lennartschoettker@hotmail.com>
2017-08-04 16:37:44 +0200

    display events that the user attends

--- angular-src/src/app/components/profile/profile.component.ts
+++ angular-src/src/app/components/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service'
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  private username: string;
+  private attendingEvents: string[]
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+    this.userService.getUserProfileData()
+      .subscribe(data => {
+        if (data.success) {
+          this.username = data.user.username;
+          this.attendingEvents = data.user.attending;
+          console.log(this.attendingEvents);
+        }
+        // Todo Error checkickg etc
+        // Todo in express app: correctly add bar to attending array in user model
+      });
   }
 
 }
