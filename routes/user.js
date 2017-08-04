9a4555598b5dc90749627381de82804b1547c39f
LS8 <lennartschoettker@hotmail.com>
2017-08-04 16:31:38 +0200

    add attend route

--- routes/user.js
+++ routes/user.js
@@ -5,10 +5,12 @@ const Router = express.Router();
 // Controllers
 const register = require('../controllers/user/register');
 const authenticate = require('../controllers/user/authenticate');
+const attend = require('../controllers/user/attend');
 const index = require('../controllers/user/index');
 
 Router.post('/register', register);
 Router.post('/authenticate', authenticate);
+Router.post('/attend', attend);
 Router.get('/', passport.authenticate('jwt', { session: false }), index);
 
 module.exports = Router;
