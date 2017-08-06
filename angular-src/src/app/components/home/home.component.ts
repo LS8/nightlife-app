570487e1a52ccfa1ddf5ca1d5f0046cfb8534f68
LS8 <lennartschoettker@hotmail.com>
2017-08-06 10:15:47 +0200

    add possibility to search by location, the user then gets redirect to the dynamic location route that displays the results

--- angular-src/src/app/components/home/home.component.ts
+++ angular-src/src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  private searchTerm: string;
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  onSearch() {
+    if (!this.searchTerm || !this.searchTerm.trim().length) {
+      // display flash message to please enter something
+      this.searchTerm = '';
+      console.log('please enter somth');
+    } else {
+      this.router.navigate([`location/${this.searchTerm}`]);
+    }
+    // this.barService.getBarByLocation('hannover')
+    //   .subscribe(data => {
+    //     console.log(data);
+    //   })
   }
 
 }
