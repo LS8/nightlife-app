60b3a9388c71238f6dd06dfd9188b2db0b342ec3
LS8 <lennartschoettker@hotmail.com>
2017-08-06 10:11:49 +0200

    add dynamic route that searches for bars by location

--- routes/bar.js
+++ routes/bar.js
@@ -3,10 +3,12 @@ const Router = express.Router();
 
 // Controllers
 const getAttendees = require('../controllers/bar/getAttendees');
+const byLocation = require('../controllers/bar/byLocation');
 const addBar = require('../controllers/bar/addBar');
 const addUserToBar = require('../controllers/bar/addUserToBar');
 const removeUserFromBar = require('../controllers/bar/removeUserFromBar');
 
+Router.get('/location/:location', byLocation);
 Router.post('/', addBar);
 Router.get('/:barId', getAttendees);
 Router.post('/attend', addUserToBar);
