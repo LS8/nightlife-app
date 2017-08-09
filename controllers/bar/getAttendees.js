f17df35b4b386ec6f3279c2fb96baefd4b2e9011
LS8 <lennartschoettker@hotmail.com>
2017-08-09 15:34:55 +0200

    return empty array when not attendees are found

--- controllers/bar/getAttendees.js
+++ controllers/bar/getAttendees.js
@@ -1,14 +1,14 @@
 const Bar = require('../../models/Bar');
 
 module.exports = (req, res) => {
-  Bar.findAttendees(req.params.barId, (err, bar) => {
+  Bar.findAttendees(req.body.barId, (err, bar) => {
     if (err) {
       res.json({ success: false, msg: 'Error while querying bar data' });
     } else if (bar) {
       const attendees = bar.attendees;
       res.json({ success: true, msg: 'Bar data retrieved successfully', attendees });
     } else {
-      const attendees = [""];
+      const attendees = [];
       res.json({ success: false, msg: 'Error nothing found', attendees });
     }
   });
