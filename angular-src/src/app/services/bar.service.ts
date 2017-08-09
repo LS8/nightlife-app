b6e8c7bf43eea15042c5f2bc20d82e9e180741f3
LS8 <lennartschoettker@hotmail.com>
2017-08-09 15:36:29 +0200

    basically add crud methods

--- angular-src/src/app/services/bar.service.ts
+++ angular-src/src/app/services/bar.service.ts
@@ -5,7 +5,7 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class BarService {
-  private url: string = 'http://localhost:8080/api/bar/location';
+  private url: string = 'http://localhost:8080/api/bar';
 
   constructor(
     private http: Http
@@ -13,8 +13,40 @@ export class BarService {
 
   getBarByLocation(location: string) {
     let headers = new Headers({ 'Content-Type': 'application/json' });
-    return this.http.get(`${this.url}/${location}`, { headers: headers })
+    return this.http.get(`${this.url}/location/${location}`, { headers: headers })
       .map(res => res.json());
   }
 
+  barAttendees(locations: object[]) {
+    for (let i = 0; i < locations.length; i++) {
+      return this.attendeesByBarId(locations[i]);
+    }
+  }
+
+  attendeesByBarId(location) {
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    return this.http.post(`${this.url}/attendees`, { barId: location.id }, { headers: headers })
+      .map(res => res.json());
+  }
+
+  addUserToBar(barId) {
+    let username = this.getUsernameFromLocalStorage();
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    return this.http.post(`${this.url}/attend`, { barId: barId, username: username }, { headers: headers })
+      .map(res => res.json());
+  }
+
+  removeUserFromBar(barId) {
+    let username = this.getUsernameFromLocalStorage();
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    return this.http.put(`${this.url}/cancel`, { barId: barId, username: username }, { headers: headers })
+      .map(res => res.json());
+
+  }
+
+  getUsernameFromLocalStorage() {
+    if (localStorage.getItem('_user')) {
+      return JSON.parse(localStorage.getItem('_user')).username;
+    }
+  }
 }
